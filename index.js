103afdbb1782bde88733e9c9f9b04a623c98fb1d
sonhai32 <lamsonhai1379@gmail.com>
2021-01-27 16:01:28 +0700

    use axios instead of request-promise

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const cheerio = require('cheerio');
-let request = require('request-promise');
+const axios = require('axios')
 const API_SERVER = 'http://online.ueh.edu.vn';
 let __VIEWSTATE = '';
 let __VIEWSTATEGENERATOR = ''
@@ -8,104 +8,73 @@ class UEH_Crawl{
     
     constructor(){
             
-       this.jar = request.jar() 
-        request = request.defaults({
-            headers:{
+    }
+    fetchHiddenParam(){
+        return new Promise(async (reslove, reject) =>{
+       
+            axios.default.get(API_SERVER, {headers: {
                 "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
                 "accept-language": "vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5",
                 "cache-control": "max-age=0",
                 "content-type": "application/x-www-form-urlencoded",
                 "upgrade-insecure-requests": "1",
                 "cookie": "_ga=GA1.3.558671917.1606993663; _fbp=fb.2.1606993664855.1318858175; ASP.NET_SessionId=klx4fdtul3ja3ohyuvnov0pe"
-            },
-            referrerPolicy: 'strict-origin-when-cross-origin',
-            referrer: "http://online.ueh.edu.vn/",
-            mode: "cors"
-
-        })
-    }
-
-    requestServer(data = {
-        formData: '', isTransform: false
-    }){
-        let form = {
-            uri: API_SERVER,
-            jar: this.jar,
-            method: (typeof data.formData === 'object') ? 'post' : 'get',
-            formData: data.formData,
-        }
-        if(data.isTransform) form.transform = body => cheerio.load(body)
-        return request(form)
-    }
-
-
-    fetchHiddenParam(){
-        return new Promise(async (reslove, reject) =>{
-            try{
-            
-                const $ = await this.requestServer({isTransform: true})
+            }}).then(res =>{
+                const $ = cheerio.load(res.data)
                 __VIEWSTATEGENERATOR = $('#__VIEWSTATEGENERATOR').val()
                 __EVENTVALIDATION = $('#__EVENTVALIDATION').val()
                 __VIEWSTATE = $('#__VIEWSTATE').val()
-
-
-                reslove('loaded')
-            }catch(err){
-                reject(err)
-            }
+                reslove('PARAM LOADED')
+            }).catch(err => reject(err))
         }) 
     }
 
     login(){
         return new Promise(async (reslove, reject) =>{
-            try{
-                const $ = await this.requestServer({
-                    formData:{
-                        __EVENTTARGET: 'ctl00$lbtDangnhap',
-                        __EVENTARGUMENT: '',
-                        __VIEWSTATE,
-                        __VIEWSTATEGENERATOR,
-                        __EVENTVALIDATION,
-                        ctl00$ContentPlaceHolder1$ctl00$ctl00$Role: 'rbtnStudent',
-                        ctl00$ContentPlaceHolder1$ctl00$ctl00$txtUserName: '31201023216',
-                        ctl00$ContentPlaceHolder1$ctl00$ctl00$txtPassword: 'nganha123456',
-                        ctl00$ContentPlaceHolder1$ctl00$ctl00$btLogin: 'Đăng nhập'
-                    },
-                    isTransform: true,
+            axios.default.post(API_SERVER, {
+                __EVENTTARGET: 'ctl00$lbtDangnhap',
+                __EVENTARGUMENT: '',
+                __VIEWSTATE,
+                __VIEWSTATEGENERATOR,
+                __EVENTVALIDATION,
+                ctl00$ContentPlaceHolder1$ctl00$ctl00$Role: 'rbtnStudent',
+                ctl00$ContentPlaceHolder1$ctl00$ctl00$txtUserName: '31201023216',
+                ctl00$ContentPlaceHolder1$ctl00$ctl00$txtPassword: 'nganha123456',
+                ctl00$ContentPlaceHolder1$ctl00$ctl00$btLogin: 'Đăng nhập'
 
-                })
-            
-                //if($('#lbtDangnhap').text() === 'Đăng Thoát') reject('Đăng Nhập Thất Bại');
+            }, {headers: {
+                "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+                "accept-language": "vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5",
+                "cache-control": "max-age=0",
+                "content-type": "application/x-www-form-urlencoded",
+                "upgrade-insecure-requests": "1",
+                "cookie": "_ga=GA1.3.558671917.1606993663; _fbp=fb.2.1606993664855.1318858175; ASP.NET_SessionId=klx4fdtul3ja3ohyuvnov0pe"
+            }}).then(res =>{
+                const $ = cheerio.load(res.data)
                 console.log($('#lbtDangnhap').text())
-                __VIEWSTATEGENERATOR = $('#__VIEWSTATEGENERATOR').val()
-                __EVENTVALIDATION = $('#__EVENTVALIDATION').val()
-                __VIEWSTATE = $('#__VIEWSTATE').val()
-               reslove(this.jar)
-            }
-            catch(err){
-                reject(err)
-            }
+                reslove('LOGIN')
+            }).catch(err => reject(err))
+            
         })
     }
 
     getStudentInfo(){
         return new Promise(async (reslove, reject) =>{
-            try{
-                console.log(__VIEWSTATEGENERATOR)
-                const $ = await this.requestServer({
-            
-                    pathURI: '/',
-                    formData: {
-                        __EVENTTARGET: 'ctl00$ContentPlaceHolder1$ctl00$ctl00$lnkInfo', 
-                        __EVENTARGUMENT: '',
-                        __VIEWSTATE,
-                        __VIEWSTATEGENERATOR,
-                        __EVENTVALIDATION
-
-                    },
-                    isTransform: true
-                })
-    
+            axios.default.post(API_SERVER, {
+                __EVENTTARGET: 'ctl00$ContentPlaceHolder1$ctl00$ctl00$lnkInfo', 
+                __EVENTARGUMENT: '',
+                __VIEWSTATE,
+                __VIEWSTATEGENERATOR,
+                __EVENTVALIDATION
+            }, {headers: {
+                "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+                "accept-language": "vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5",
+                "cache-control": "max-age=0",
+                "content-type": "application/x-www-form-urlencoded",
+                "upgrade-insecure-requests": "1",
+                "cookie": "_ga=GA1.3.558671917.1606993663; _fbp=fb.2.1606993664855.1318858175; ASP.NET_SessionId=klx4fdtul3ja3ohyuvnov0pe"
+            }}).then(res =>{
+                const $ = cheerio.load(res.data)
                 let user = {
                     id: $('#ContentPlaceHolder1_ctl00_ctl00_ctl00_lbMaso').text(),
                     name: $('#ContentPlaceHolder1_ctl00_ctl00_ctl00_lbHoVaTen').text(),
@@ -118,33 +87,31 @@ class UEH_Crawl{
                 }
 
                 reslove(user)
-            }catch(err){
-                reject(err)
-            }
+            }).catch(err => reject(err))
     
         })
     }
     
     getSchedule(){
         return new Promise(async (reslove, reject) =>{
-            try{
-                
-
+            axios.default.post(API_SERVER, {
+                __EVENTTARGET: 'ctl00$ContentPlaceHolder1$ctl00$ctl00$lnkThoiKhoaBieu', 
+                __EVENTARGUMENT: '',
+                __VIEWSTATE,
+                __VIEWSTATEGENERATOR,
+                __EVENTVALIDATION
+            }, {headers: {
+                "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+                "accept-language": "vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5",
+                "cache-control": "max-age=0",
+                "content-type": "application/x-www-form-urlencoded",
+                "upgrade-insecure-requests": "1",
+                "cookie": "_ga=GA1.3.558671917.1606993663; _fbp=fb.2.1606993664855.1318858175; ASP.NET_SessionId=klx4fdtul3ja3ohyuvnov0pe"
+            }}).then(res =>{
                 let listSchedule = [];
-                const $ = await this.requestServer({
-                    formData: {
-                        __EVENTTARGET: 'ctl00$ContentPlaceHolder1$ctl00$ctl00$lnkThoiKhoaBieu', 
-                        __EVENTARGUMENT: '',
-                        __VIEWSTATE,
-                        __VIEWSTATEGENERATOR,
-                        __EVENTVALIDATION
-                    },
-                    isTransform: true
-                })
-
+                const $ = cheerio.load(res.data)
                 let scheduleTable = $('#ContentPlaceHolder1_ctl00_ctl00_ctl00_grvThoikhoabieu').children('tbody').children()
                 scheduleTable.map(function(){
-
                     const columns = $(this).children();
                     listSchedule.push({
                         weekIndex: $(columns[0]).find('span').text(),
@@ -159,9 +126,8 @@ class UEH_Crawl{
                 console.log(listSchedule)
                
                 reslove('0k')
-            }catch(error){
-                reject(error)
-            }
+            }).catch(err => reject(err))
+            
         })
         
 
@@ -174,11 +140,9 @@ const API = new UEH_Crawl();
     try{
         const login = await API.fetchHiddenParam()
         const a = await API.login()
-        const user = await API.getStudentInfo()
-
-
-        console.log(user)
        const c = await API.getSchedule(null)
+        const user = await API.getStudentInfo().then(val => console.log(val))
+
         console.log(c)
     
     }catch(err){
